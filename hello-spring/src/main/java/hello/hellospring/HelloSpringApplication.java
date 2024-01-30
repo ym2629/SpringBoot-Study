cfbe05e45719c31fbc219b598f4178064d5f8807
ym2629 <dydals2629@gmail.com>
2024-01-30 14:30:53 +0900

    verify and enhance Thymeleaf Template Engine functionality

--- hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -1,7 +1,10 @@
 package hello.hellospring;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class HelloSpringApplication {
