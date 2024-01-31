7efd79a0400c6e4c02ad8e3c8c460a9dc3874a18
ym2629 <dydals2629@gmail.com>
2024-01-31 14:25:51 +0900

    test: member service2

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -6,6 +6,8 @@ import hello.hellospring.repository.MemoryMemberRepository;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -14,7 +16,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
+//    MemberService memberService = new MemberService();
+//    //이해가 안되는게 MemberService에서 따로 MemoryMemberRepository객체를 생성해서 사용하는데
+//    //왜 다른 객체에서 clearStore()을 하는데 MemberService의 MemoryMemberRepository의 내용이 삭제되지?
+//    //Map이 static으로 선언되어 있어서 문제가 안되는거라는데? -> 근데 좀 껄끄러운 거라고 말하심 -> static이면 같은 db가 되나봐
+//    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+    MemberService memberService;
+    MemoryMemberRepository memoryMemberRepository;
+
+    @BeforeEach
+    public void beforeEach(){
+        memoryMemberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memoryMemberRepository);
+    }
+    @AfterEach
+    public void afterEach(){
+        memoryMemberRepository.clearStore();
+    }
 
     @Test
     void join() {
