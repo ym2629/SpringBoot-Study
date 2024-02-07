2fc280f1eb5fbcf5b7aaaae9aacbf663121778a5
ym2629 <dydals2629@gmail.com>
2024-02-07 13:04:52 +0900

    start JPA project

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,8 @@
+package hellojpa;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+    }
+}
--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -7,18 +7,23 @@ import hello.hellospring.service.MemberService;
 import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
     private EntityManager em;
-//    private final DataSource dataSource;
+
+    //    private final DataSource dataSource;
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
+
     @Bean
     public MemberRepository memberRepository() {
 // return new MemoryMemberRepository();
