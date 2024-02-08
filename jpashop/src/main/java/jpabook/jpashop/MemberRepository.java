efe9cd287795854cfbdd3727a07424a5bc9ad8b4
ym2629 <dydals2629@gmail.com>
2024-02-08 21:03:12 +0900

    test: connect to DB and find Entity

---
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository //스프링이 자동으로 생성해줌. 스프링 빈으로 등록. �
+public class MemberRepository {
+    // 스프링 컨테이너에서 엔티티 매니저 주입
+    @PersistenceContext
+    EntityManager em;
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+}
