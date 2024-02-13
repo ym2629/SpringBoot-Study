0d8328541b3d041988e574503040dc14fa9a2447
ym2629 <dydals2629@gmail.com>
2024-02-13 15:30:59 +0900

    feat: item Entity with business logic

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,10 +1,10 @@
 package jpabook.jpashop.domain;
 
+import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import org.springframework.boot.web.servlet.filter.OrderedRequestContextFilter;
 
 @Entity
 @Table(name = "order_item")
